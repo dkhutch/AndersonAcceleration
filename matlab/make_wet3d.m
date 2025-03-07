c8eb06e367320d59f70a7e3992ea827d1b0256e3
dkhutch <david.hutchinson@unsw.edu.au>
2025-03-07 16:22:19 +1100

    update wet3d

--- matlab/make_wet3d.m
+++ matlab/make_wet3d.m
@@ -1,5 +1,5 @@
 % make a logical mask for all ocean grid cells that are not NaN.
-infile='/scratch/y99/dkh157/access-esm/archive/pi_dh-pi_dh-b95a5a20/output000/ocean/ocean-3d-pot_temp-1monthly-mean-ym_0101_01.nc';
+infile='/scratch/y99/dkh157/access-om2/archive/caco3dyn_1deg/output000/ocean/ocean-3d-pot_temp-1-monthly-mean-ym_1900_01.nc';
 temp = ncread(infile, 'pot_temp');
 t1 = squeeze(temp(:,:,:,1));
 wet3d = ~isnan(t1);
